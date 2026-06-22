2d72b0698cfbee464e7bffa475b73d59cf7bde74
leozdin <addunoo165@gmail.com>
2026-06-23 00:39:09 +0500

    Delete Webtoon-Downloader/webtoon-downloader directory

--- Webtoon-Downloader/webtoon-downloader/start.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting Webtoon Downloader...
-cd /d "C:\Users\hp\Documents\Webtoon-Downloader\webtoon-downloader\backend"
-call "C:\Users\hp\Documents\Webtoon-Downloader\.venv\Scripts\activate.bat"
-start "" python app.py
-timeout /t 4 /nobreak
-start "" "C:\Users\hp\Documents\Webtoon-Downloader\webtoon-downloader\frontend\index.html"
-pause
